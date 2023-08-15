1231c3fbe753fd2989c0e70aa74f3af27dab1eb0
Victor Socaciu <victor.socaciu@bp.com>
2023-08-15 11:24:39 +0400

    Initial Commit

---
+++ VoiceCommands/VoiceCommandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceCommandsApp.swift
+//  VoiceCommands
+//
+//  Created by Victor Socaciu on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct VoiceCommandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
